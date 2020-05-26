c10ecb055d5d889ac1ca9fadf19f3e07b27e441a
afrozalam00 <65973734+afrozalam00@users.noreply.github.com>
2020-05-27 04:48:38 +0500

    Create HmacMD5Demo.cs
    for md5 hash conversion.

---
+++ Project/CryptoUtilsConsole/hashing/HmacMD5Demo.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace CryptoUtilsConsole.hashing
+{
+    class HmacMD5Demos
+    {
+        public static byte[] getHMACMD5(string message)
+        {
+            using (var randonNumberGenerator = new RNGCryptoServiceProvider())
+            {
+                byte[] key = new byte[32];
+                randonNumberGenerator.GetBytes(key);
+                using (var hmacmd5 = new HMACMD5(key))
+                {
+                    byte[] hashmeHashed = hmacmd5.ComputeHash(Encoding.UTF8.GetBytes(message));
+                    return hashmeHashed;
+                }
+            }
+        }
+
+        public static void LaunchDemo()
+        {
+            Console.WriteLine(" Hmac MD5 HASH RUNNING ");
+
+            Console.WriteLine("_________________________");
+
+            Console.WriteLine("INPUT HERE :");
+
+            Console.WriteLine("");
+
+            string message = Console.ReadLine();
+
+            byte[] hashmeHashed = getHMACMD5(message);
+            string result = BitConverter.ToString(hashmeHashed).Replace("-", "");
+
+            Console.WriteLine("HMACMD5 : " + result);
+            Console.WriteLine("------------------");
+            
+        }
+    }
+}
