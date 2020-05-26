1869ad0dc8ffc93dc096cf21f0a14c353b6f4442
Wajahat Ali Khalid <57641740+IamAli9@users.noreply.github.com>
2020-05-27 03:06:58 +0500

    Now this program.cs file call  contributers work

--- Project/CryptoUtilsConsole/Program.cs
+++ Project/CryptoUtilsConsole/Program.cs
@@ -13,7 +13,24 @@ namespace CryptoUtilsConsole
     {
         static void Main(string[] args)
         {
-         
+            // WARNING
+            // the below code is only meant to indicate you where the snippets you are looking
+            // for are, if you run this code as is, the app crashes because it will not find a the hardcoded
+            // file path to encrypt Hashing
+            HmacMD5Demos.LaunchDemo();
+            HmacSHA1Demos.LaunchDemo();
+            Md5Demos.LaunchDemo();
+            Sha1Demos.LaunchDemo();
+            Sha256Demos.LaunchDemo();
+            Sha512Demos.LaunchDemo();
+
+            //// Symmetric crypto
+            AESDemos.LaunchDemo();
+            DESDemos.LaunchDemo();
+            DESDemos.LaunchFileDemo();
+            TripleDESDemos.LaunchDemo();
+
+
             // Asymmetric crypto
             RSACspDemo.LaunchDemo();
             RsaFileDemo.LaunchDemo();
