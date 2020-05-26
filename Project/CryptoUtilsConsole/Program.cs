d8b072c1abf382d28471f2f32e348d3e29400ea4
faizansidd97 <55597848+faizansidd97@users.noreply.github.com>
2020-05-27 03:41:48 +0500

    Add files via upload

---
+++ Project/CryptoUtilsConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using CryptoUtilsConsole.symmetric_crypto;
+
+namespace CryptoUtilsConsole
+{
+    public enum CryptoOperation
+    {
+        ENCRYPT,
+        DECRYPT
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            //// Symmetric crypto
+             AESDemos.LaunchDemo();
+            DESDemos.LaunchDemo();
+            DESDemos.LaunchFileDemo();
+            TripleDESDemos.LaunchDemo();
+
+
+           
+        }
+    }
+}
