27b8fb158b71f7d9691933f22ebdbdc2dc804f3a
Wajahat Ali Khalid <57641740+IamAli9@users.noreply.github.com>
2020-05-27 03:03:50 +0500

    First task rsa addition
    RSA  csp demo
    RSA file demo
    RSA memory 
    
    done

---
+++ Project/CryptoUtilsConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using CryptoUtilsConsole.AsymmetricCrypto;
+
+
+namespace CryptoUtilsConsole
+{
+    public enum CryptoOperation
+    {
+        ENCRYPT,
+        DECRYPT
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            // Asymmetric crypto
+            RSACspDemo.LaunchDemo();
+            RsaFileDemo.LaunchDemo();
+            RsaInMemoryDemo.LaunchDemo();
+        }
+    }
+}
